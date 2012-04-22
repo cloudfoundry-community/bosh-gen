9e78f60b226ef0acb6dc39338f4fdd55dfe33f69
Dr Nic Williams <drnicwilliams@gmail.com>
2012-04-21 22:19:08 -0700

    generate .gitignore

--- lib/bosh/gen/generators/new_release_generator.rb
+++ lib/bosh/gen/generators/new_release_generator.rb
@@ -13,13 +13,37 @@ module Bosh::Gen
       
       def directories
         %w[jobs packages src blobs].each do |dir|
-          directory dir, File.join(name, dir)
+          directory dir, gen_path(dir)
         end
       end
       
       def blobs_yaml
         blobs = {}
-        create_file File.join(name, "blob_index.yml"), YAML.dump(blobs)
+        create_file gen_path("blob_index.yml"), YAML.dump(blobs)
+      end
+      
+      def git_init
+        create_file gen_path(".gitignore"), <<-IGNORE.gsub(/^\s{8}/, '')
+        config/dev.yml
+        config/private.yml
+        releases/*.tgz
+        dev_releases
+        blobs
+        .dev_builds
+        .idea
+        .DS_Store
+        .final_builds/jobs/**/*.tgz
+        .final_builds/packages/**/*.tgz
+        *.swp
+        *~
+        *#
+        #*
+        IGNORE
+      end
+      
+      private
+      def gen_path(path)
+        File.join(name, path)
       end
     end
   end
