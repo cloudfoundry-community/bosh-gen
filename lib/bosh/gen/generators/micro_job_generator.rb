acbd9cd571169ec7f7058dc4bfd2dbc29ec1bfab
Dr Nic Williams <drnicwilliams@gmail.com>
2012-10-17 09:12:21 -0700

    Don't gitignore jobs/micro/prepare_spec & prepare
    Previously, jobs/micro* meant the entire folder was ignored.
    What we wanted is to ensure jobs/micro/prepare_spec & prepare
    are in repo, but nothing else.

--- lib/bosh/gen/generators/micro_job_generator.rb
+++ lib/bosh/gen/generators/micro_job_generator.rb
@@ -17,27 +17,27 @@ module Bosh::Gen
       end
       
       def create_job
-        directory "jobs/micro"
-        chmod "jobs/micro/prepare", 0755
+        directory "jobs/micro", "jobs/#{job_name}"
+        chmod "jobs/#{job_name}/prepare", 0755
       end
       
       def prepare_spec_defaults_all_jobs
-        jobs = Dir[File.expand_path("jobs/*")].map {|job| File.basename(job) } - ['micro']
+        jobs = Dir[File.expand_path("jobs/*")].map {|job| File.basename(job) } - [job_name]
         spec = { "jobs" => jobs }
-        create_file "jobs/micro/prepare_spec", YAML.dump(spec)
+        create_file "jobs/#{job_name}/prepare_spec", YAML.dump(spec)
       end
       
       def gitignore
         append_file ".gitignore", <<-IGNORE.gsub(/^\s{8}/, '')
-        jobs/micro*/monit
-        jobs/micro*/spec
-        jobs/micro*/templates/
+        jobs/#{job_name}/monit
+        jobs/#{job_name}/spec
+        jobs/#{job_name}/templates/
         IGNORE
       end
       
       def readme
         say ""
-        say "Edit "; say "jobs/micro/prepare_spec ", :yellow
+        say "Edit "; say "jobs/#{job_name}/prepare_spec ", :yellow
           say "with ordered list of jobs to include"
         say "in micro job. The order of jobs implicitly specifies the order in"
         say "which they are started."
@@ -45,6 +45,11 @@ module Bosh::Gen
         say ""
       end
       
+    private
+      # May be an argument for the generator in future
+      def job_name
+        "micro"
+      end
     end
   end
 end
