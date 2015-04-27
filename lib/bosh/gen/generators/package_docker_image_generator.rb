0e7bd723d779a4d3af0f82fb5d459adb76a85197
Dr Nic Williams <drnicwilliams@gmail.com>
2015-04-27 09:03:42 -0700

    wip - package --docker-image

---
+++ lib/bosh/gen/generators/package_docker_image_generator.rb
@@ -0,0 +1,62 @@
+require 'yaml'
+require 'thor/group'
+
+# for the #sh helper
+require 'rake'
+require 'rake/file_utils'
+
+module Bosh::Gen
+  module Generators
+    class PackageDockerImageGenerator < Thor::Group
+      include Thor::Actions
+      include FileUtils
+
+      argument :name
+      argument :docker_image
+
+      BLOB_FILE_MIN_SIZE=20_000 # files over 20k are blobs
+
+      def self.source_root
+        File.join(File.dirname(__FILE__), "package_docker_image_generator", "templates")
+      end
+
+      def check_root_is_release
+        unless File.exist?("jobs") && File.exist?("packages")
+          raise Thor::Error.new("run inside a BOSH release project")
+        end
+      end
+
+      def check_name
+        raise Thor::Error.new("'#{name}' is not a valid BOSH id") unless name.bosh_valid_id?
+      end
+
+      def docker_save
+        mkdir_p("blobs/docker-images")
+        sh "docker pull #{docker_image}"
+        sh "docker save #{docker_image} > blobs/docker-images/#{image_filename}"
+      end
+
+      def copy_files
+        directory "jobs"
+        directory "packages"
+      end
+
+      private
+      def package_name
+        name
+      end
+
+      def image_name
+        docker_image.gsub(/\W+/, '_')
+      end
+
+      def image_filename
+        "#{image_name}.tgz"
+      end
+
+      def job_name
+        "#{image_name}_image"
+      end
+    end
+  end
+end
---
+++ lib/bosh/gen/generators/package_docker_image_generator/templates/jobs/%job_name%/templates/helpers/ctl_setup.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# Setup env vars and folders for the ctl script
+# This helps keep the ctl script as readable
+# as possible
+
+# Usage options:
+# source /var/vcap/jobs/foobar/helpers/ctl_setup.sh JOB_NAME OUTPUT_LABEL
+# source /var/vcap/jobs/foobar/helpers/ctl_setup.sh foobar
+# source /var/vcap/jobs/foobar/helpers/ctl_setup.sh foobar foobar
+# source /var/vcap/jobs/foobar/helpers/ctl_setup.sh foobar nginx
+
+set -e # exit immediately if a simple command exits with a non-zero status
+set -u # report the usage of uninitialized variables
+
+JOB_NAME=$1
+output_label=${1:-JOB_NAME}
+
+export JOB_DIR=/var/vcap/jobs/$JOB_NAME
+chmod 755 $JOB_DIR # to access file via symlink
+
+source $JOB_DIR/helpers/ctl_utils.sh
+redirect_output ${output_label}
+
+export HOME=${HOME:-/home/vcap}
+
+# Add all packages' /bin & /sbin into $PATH
+for package_bin_dir in $(ls -d /var/vcap/packages/*/*bin)
+do
+  export PATH=${package_bin_dir}:$PATH
+done
+
+# Setup log, run and tmp folders
+
+export RUN_DIR=/var/vcap/sys/run/$JOB_NAME
+export LOG_DIR=/var/vcap/sys/log/$JOB_NAME
+export TMP_DIR=/var/vcap/sys/tmp/$JOB_NAME
+export STORE_DIR=/var/vcap/store/$JOB_NAME
+for dir in $RUN_DIR $LOG_DIR $TMP_DIR $STORE_DIR
+do
+  mkdir -p ${dir}
+  chown vcap:vcap ${dir}
+  chmod 775 ${dir}
+done
+export TMPDIR=$TMP_DIR
+
+PIDFILE=$RUN_DIR/$JOB_NAME.pid
